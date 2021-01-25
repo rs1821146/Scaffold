7214c0b8badd73d4784352273074771ebf2d0081
HushTec <rabisingson@gmail.com>
2021-01-25 20:43:00 +0800

    added

--- index.php
+++ index.php
@@ -4,25 +4,28 @@
 <head>
      <meta charset="UTF-8">
      <title> Home Page </title>
+</head>
+
+<head>
     <style>
 body{
-    background-image: url("https://businessjournaldaily.com/wp-content/uploads/2019/10/unemployment-graphic-01-1.jpg");
+    background-image: url("images/background.jfif");
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-size: cover;
 }
 .navbar{
-  height: 50px;
-  width: 80%;
+  height: 70px;
+  width: 70%;
   font color: black;
   float: right;
-  background-color: white;
+  background-color: grey;
 }
 </style>
 </head>
 <div class="navbar">
   <p align="right">
-    <font face='Inconsolata' size='5' color="black">
+    <font face='Times new roman' size='5' color="grey">
            <a href="#"> HOME </a>&nbsp;&nbsp;&nbsp;&nbsp;
            <a href="#"> ABOUT </a>&nbsp;&nbsp;&nbsp;
            <a href="#"> SUPPORT </a>&nbsp;&nbsp;&nbsp;
@@ -32,8 +35,32 @@ body{
    <h3 align="left">
      <div class="wrapper">
      <div class="logo">
-     <img src = "images/Here to Help.jpg" width = 300, length = 300>
+     <img src = "images/Here to Help.jpg" width = 400, length = 400>
    <h3>
+     <br><br><br>
+     <p align="center">
+     <img src="images/Unemployment.jpg" style="width:100%;max-width:1000px;margin-top:32px;">
+<font face='Times new roman' size='5' color="grey">
+<p><p><p align=center>Unemployment eases to 8.7% in October 2020, but workers looking for more jobs
+<br>DEC 3, 2020 10:00 AM PHT
+<br>RALF RIVAS</p align=center>
+<p><p align=center>The number of jobless Filipinos goes down in October, but the number of employed people who are still looking for extra jobs remains high
+<br>The Philippines' unemployment rate eased to 8.7% in October, equivalent to around 3.8 million jobless Filipinos amid the coronavirus crisis.
+<br>The latest figure reported by the Philippine Statistics Authority on Thursday, December 3, is lower than the 10% reported in July and the all-time high of 17.6% last April.
+<br>At the regional level, 3 regions still reported double-digit unemployment rates. These are:
+<br>National Capital Region - 12.4%
+<br>Ilocos Region - 11.5%
+<br>Calabarzon - 11%
+<br>Before the pandemic, the unemployment rate was only at 5.3% in January, or 2.4 million people, and 5.1% in April 2019, or 2.3 million people.
+<br>While the number of jobless Filipinos went down, underemployment, or the number of people who are employed but are looking for more work to meet needs, stood at 14.4% in October.
+<br>October underemployment was lower than the rates in April and July at 18.9%, and 17.3%, respectively. However, the underemployment rate in October 2020 is still higher than the October 2019 rate of 12.8%.
+<br>The labor force participation rate (LFPR) in October 2020 was 58.7% or 43.6 million Filipinos who were either employed or unemployed.
+<br>This is the second lowest LFPR reported in the history of the Philippine labor market following the record low of 55.7% reported in April 2020. In October 2019, the LFPR was at 61.4%, translating to 44.6 million Filipinos.
+<br>The Philippine government is expecting more jobs to return in 2021, but also admits that millions of Filipinos who have fallen into poverty due to the coronavirus pandemic will remain poor next year.
+<br>Acting Socioeconomic Planning Secretary Karl Chua earlier said the poverty rate will likely range from 15.5% to 17.5% in 2021 from the 16.6% in 2018. The projected range is higher than the economic team's target of bringing poverty down to 14%.
+<br>Before the pandemic, the Philippines was poised to be ranked as an upper-middle income country in 2020.
+</p align=center>
+</font>
 
 </body>
 </html>
